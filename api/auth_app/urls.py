d7afd15e3140db2fa1b13933313649b397ab5bbd
Noah Mustoe <62711423+cobradev4@users.noreply.github.com>
2022-09-24 16:52:33 -0500

    simplify endpoints

--- api/auth_app/urls.py
+++ api/auth_app/urls.py
@@ -8,9 +8,9 @@ from rest_framework_simplejwt.views import (
 from .views import RegisterApi, UserApi
 
 urlpatterns = [
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/register/', RegisterApi.as_view(), name='register_user'),
-    path('api/user/', UserApi.as_view(), name='current_user')
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('register/', RegisterApi.as_view(), name='register_user'),
+    path('user/', UserApi.as_view(), name='current_user')
 ]
--- api/data_app/urls.py
+++ api/data_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from .views import ThreatApi
 
 urlpatterns = [
-    path('api/threat-data/', ThreatApi.as_view(), name='threat-data')
+    path('threat-data/', ThreatApi.as_view(), name='threat-data')
 ]
