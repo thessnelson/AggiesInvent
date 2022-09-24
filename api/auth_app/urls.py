9a90d81030b2b75fe34fd2a37db59f8911f42f1c
Noah Mustoe <62711423+cobradev4@users.noreply.github.com>
2022-09-24 14:59:16 -0500

    add endpoint to get current user info

--- api/auth_app/urls.py
+++ api/auth_app/urls.py
@@ -5,11 +5,12 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
-from .views import RegisterApi
+from .views import RegisterApi, UserApi
 
 urlpatterns = [
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/register/', RegisterApi.as_view(), name='register_user')
+    path('api/register/', RegisterApi.as_view(), name='register_user'),
+    path('api/user/', UserApi.as_view(), name='current_user')
 ]
--- api/auth_app/views.py
+++ api/auth_app/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions, mixins
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 from .serializers import RegisterSerializer, UserSerializer
 from django.contrib.auth.models import User
@@ -16,3 +16,20 @@ class RegisterApi(generics.GenericAPIView):
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "message": "User created successfully.",
         })
+
+class UserApi(generics.GenericAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
+
+    def get(self, request, *args, **kwargs):
+        user = self.get_queryset()[0]
+        serializer = self.serializer_class(user)
+
+        return Response({
+            "user": serializer.data,
+            "message": "User retrieved successfully.",
+        })
+1
