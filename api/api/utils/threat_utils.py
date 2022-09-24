1f6e5a88baf733a9c05a1a5ef982aed352ac07c1
Noah Mustoe <62711423+cobradev4@users.noreply.github.com>
2022-09-24 15:20:46 -0500

    anomalies

--- api/api/utils/threat_utils.py
+++ api/api/utils/threat_utils.py
@@ -1,5 +1,5 @@
-def get_threats():
-    threats = [
+def get_anomalies():
+    anomalies = [
         {
             'flow_duration': 0,
             'delta_idle': 0,
@@ -24,14 +24,6 @@ def get_threats():
             'ttl_len_back_packets': 0,
             'score': 0
         },
-        {
-            'flow_duration': 0,
-            'delta_idle': 0,
-            'ttl_fwd_packets': 0,
-            'ttl_back_packets': 0,
-            'ttl_len_back_packets': 0,
-            'score': 0
-        }
     ]
-
-    return threats
+            
+    return anomalies
