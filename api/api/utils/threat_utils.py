f0637d9a231e2a038dbdccb621b9b0616ced0b66
Noah Mustoe <62711423+cobradev4@users.noreply.github.com>
2022-09-24 18:46:23 -0500

    Update gitignore, more data chosen randomly

--- api/api/utils/threat_utils.py
+++ api/api/utils/threat_utils.py
@@ -1,11 +1,16 @@
 import pandas as pd
+import random
 from math import log, ceil
 
 def get_anomalies():
-    anomalies = pd.read_csv('./data/ExampleData.csv', header=0).to_dict('records')
+    anomalies = pd.read_csv('./data/anomaly_scores.csv', header=0).to_dict('records')
 
     # convert score to scale of 1-10
     for a in anomalies:
-        a['score'] = ceil(log(a['score'], 100))
+        if a['score'] > 0:
+            a['score'] = ceil(log(a['score'], 100))
+        elif a['score'] < 0:
+            a['score'] = 0
     
-    return anomalies
+    # choose 100 random anomalies
+    return random.sample(anomalies, 100)
